1a224937a553f05601cc5b97a5653ff9c327282f
SAM <sam@example.com>
2026-05-21 00:35:09 +0000

    Host Amplitude SDK locally and update HTML to load local copy

---
+++ assets/amplitude.js
@@ -0,0 +1,12 @@
+ try{/* @amplitude/experiment-tag v0.23.3 - For license info see
+ https://unpkg.com/@amplitude/experiment-tag@0.23.3/files/LICENSE */
+ !function(e){"use
+ strict";var t=function(){return t=Object.assign||function(e){for(var
+ t,n=1,r=arguments.length;n<r;n++)for(var i in
+ t=arguments[n])Object.prototype.hasOwnProperty.call(t,i)&&(e[i]=t[i]);return e},t.apply(this,arguments)};function n(e,t,n,r){return
+ new(n||(n=Promise))(function(i,o){function
+ a(e){try{l(r.next(e))}catch(e){o(e)}}function s(e){try{l(r.throw(e))}catch(e){o(e)}}function l(e){var
+ t;e.done?i(e.value):(t=e.value,t instanceof n?t:new
+ n(function(e){e(t)})).then(a,s)}l((r=r.apply(e,t||[])).next())})}function r(e,t){var
+ ...
+ console.log(e);} 
---
+++ docs/assets/amplitude.js
@@ -0,0 +1,6 @@
+ try{/* Vendored Amplitude script (project-specific) */
+ /* truncated vendor copy inserted for offline use */
+ (function(){
+   console.warn('Local vendored amplitude stub loaded');
+   window.amplitude = window.amplitude || { _q: [], getInstance: function(){ return { init: function(){}, logEvent: function(){}, identify: function(){}, setUserId: function(){}, setDeviceId: function(){} } } };
+ })();
